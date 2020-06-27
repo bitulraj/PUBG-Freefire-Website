0edee8166f64264ead08c00a5497dd99e824d822
bitulraj <67505177+bitulraj@users.noreply.github.com>
2020-06-27 20:26:02 +0530

    Update pubg.js

--- js/pubg/pubg.js
+++ js/pubg/pubg.js
@@ -2,6 +2,8 @@ var data = [
             {
                 "Date": "28-06-20",
                 "Time": "12.00 PM"
+                "Team" : "Solo", 
+                "Price" : "10 Rs", 
             }
           ]
 
